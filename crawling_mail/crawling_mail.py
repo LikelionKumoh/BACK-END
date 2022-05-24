a0887174de20d0099c96e0c0d1f862ac3f53e8b5
Raccooon98 <redbull1171@gmail.com>
2022-05-25 00:54:26 +0900

    메일보내기 과제 수정본

--- crawling_mail/crawling_mail.py
+++ crawling_mail/crawling_mail.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 from email.message import EmailMessage
+import re
 
 header={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'}
 
@@ -25,17 +26,31 @@ for meta in metas:
     num += 1
 text_file.close()
 
-# SMTP_SERVER = "smtp.gmail.com"
-# SMTP_PORT = 465
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
 
-# message = EmailMessage()
-# message.set_content("코드라이언 수업중입니다.")
+def sendEmail(addr):
+    reg = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
+    if bool(re.match(reg,addr)):
+        smtp.send_message(message)
+        print("정상적으로 메일이 발송되었습니다.")
+    else:
+        print("유효한 이메일주소가 아닙니다.")
 
-# message["Subject"] = "롤토체스 추천메타 목록입니다."
-# message["From"] = "redbull1171@gmail.com"
-# message["To"] = "junbs1498@naver.com"
 
-# smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
-# smtp.login("redbull1171@gmail.com","##########")
-# smtp.send_message(message)
-# smtp.quit()
+message = EmailMessage()
+message.set_content("롤토체스 추천 덱 목록을 크롤링 한 결과 입니다.")
+
+message["Subject"] = "전용현 크롤링 과제입니다."
+message["From"] = "#######@likelion.org"
+message["To"] = "kit@likelion.org"
+
+with open("tft_meta.txt","rb") as txt_file:
+    txt=txt_file.read()
+
+message.add_attachment(txt,maintype='txt', subtype='txt', filename= txt_file.name)
+
+smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
+smtp.login("#######@likelion.org","#######")
+sendEmail(message["To"])
+smtp.quit()

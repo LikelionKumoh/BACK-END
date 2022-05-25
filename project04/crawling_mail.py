016f09787ab6c64fa1cbb60138dab36db2d02d4a
eunbin <leb001122@icloud.com>
2022-05-25 19:35:36 +0900

    220525_v2

--- project04/crawling_mail.py
+++ project04/crawling_mail.py
@@ -10,28 +10,32 @@ header = {
     'User-Agent' : 'https://www.whatismybrowser.com/detect/what-is-my-user-agent/'
 }
 
-url = "https://www.google.com/search?q=최예나"
+url = "https://search.naver.com/search.naver?query=최예나"
 
 response = requests.get(url, headers=header)
 soup = BeautifulSoup(response.text, 'html.parser')
-html_file = open('crawling.txt', "w", encoding='utf-8')
-html_file.write(response.text)
-html_file.close()
+title = soup.select_one('#main_pack > section.sc_new.cs_common_module.case_normal._au_people_content_wrap.color_5 > div.cm_content_wrap > div:nth-child(4) > div > div.cm_info_box.scroll_img_album._tab_content > div > div > div > ul > li:nth-child(1) > div > div > strong > a').get_text()
+print(title)
+txt_file = open('crawling.txt', "w", encoding='utf-8')
+txt_file.write(title)
+txt_file.close()
+
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
 
+
 message = EmailMessage()
 message.set_content("최예나 smiley 넘 좋아요")
 message["Subject"] = "크롤링해서 메일보내기[이은빈]"
-message["From"] = "kit@likelion.org"
-message["To"]= "leeeunbin001122@gmail.com"
+message["From"] = "leeeunbin001122@gmail.com"
+message["To"]= "kit@likelion.org"
 
 with open('crawling.txt', 'rb') as f:
     message.add_attachment(f.read(), maintype='text', subtype="plain", filename=f.name)
 
 smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
-smtp.login("leeeunbin001122@gmail.com", "#")
+smtp.login("leeeunbin001122@gmail.com", "냠냠")
 smtp.send_message(message)
 smtp.quit()
 

8b219592262ae75d62fde60f745fc4513a52a514
hxeyexn <kck85075@kumoh.ac.kr>
2022-07-03 13:15:11 +0900

    의류 검색 포털 미완성

--- 의류 검색 포털/ClothingSearch/crawler/views.py
+++ 의류 검색 포털/ClothingSearch/crawler/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import ClothingData
+from crawler.models import ClothingData
+from musinsa_crawling import clothing_data
 
 def home(request):
     return render(request, 'index.html')
     
 def create(request):
     crawlingMusinsa = ClothingData.objects.all()
-    crawlingMusinsa.delete()
-    for data in crawling:
+    crawlingMusinsa.delete()   
+    musinsa_data = clothing_data()
+    for data in musinsa_data:
         item = ClothingData()
         item.rank = data['rank']
         item.image = data['image']

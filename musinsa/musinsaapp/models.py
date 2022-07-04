98ddc68b600070f03746ae3b447f9bd7f931d2ef
jinjunho <52707417+junho5343@users.noreply.github.com>
2022-07-04 15:27:11 +0900

    Delete musinsa directory

--- musinsa/musinsaapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    rank = models.CharField(max_length=10)
-    img = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
-    price = models.CharField(max_length=200) 

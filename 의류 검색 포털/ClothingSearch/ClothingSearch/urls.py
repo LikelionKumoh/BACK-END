3fec8d531159710c6e9a47f53355b9f23304087f
hxeyexn <kck85075@kumoh.ac.kr>
2022-07-21 04:27:21 +0900

    의류 검색 포털 파일 삭제

--- 의류 검색 포털/ClothingSearch/ClothingSearch/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from crawler import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-]
--- 의류 검색 포털/ClothingSearch/crawler/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from crawler.models import ClothingData
-
-admin.site.register(ClothingData)
